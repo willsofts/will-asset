763140708b78b5d8eee52b63e66d6670eb60cc41
tassun <32536248+tassun@users.noreply.github.com>
2024-09-11 10:15:27 +0700

    change icon

--- js/bc.js
+++ js/bc.js
@@ -5,10 +5,16 @@ function startReceiveBroadcast() {
         socket.on('broadcast-message', function(msg) {
             console.log("broadcast-message:",msg);
             let div = $("<div class='bc-layer'></div>");
-            let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("&times;");
+            let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
             link.click(function() { div.remove(); });
             let span = $("<span></span>").html(msg.message);
-            div.append(link).append(span).appendTo($("body"));
+            let body = $("body");
+            let isz = body.find("div.bc-layer").length;
+            if(isz > 0 && isz < 10) {
+                let bottom = isz * 50;
+                div.css({bottom: bottom});
+            }
+            div.append(link).append(span).appendTo(body);
         });
     }
 }
